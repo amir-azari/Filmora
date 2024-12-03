4e44b070d26db57f7700782399f7360016012a0f
amir <ah.azari.wr@gmail.com>
2024-12-03 09:18:15 +0330

    feat: Implement RemoteDataSource class for API calls

---
+++ app/src/main/java/azari/amirhossein/filmora/data/source/RemoteDataSource.kt
@@ -0,0 +1,12 @@
+package azari.amirhossein.filmora.data.source
+
+import azari.amirhossein.filmora.data.network.ApiServices
+import azari.amirhossein.filmora.models.authentication.RequestLogin
+import azari.amirhossein.filmora.models.authentication.RequestSession
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(private val api : ApiServices) {
+    suspend fun requestToken() = api.requestToken()
+    suspend fun validateWithLogin( request: RequestLogin) = api.validateWithLogin(request)
+    suspend fun createSession( request: RequestSession) = api.createSession(request)
+}
