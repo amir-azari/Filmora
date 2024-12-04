fcc122ca33ca294c0e81af22257fa0080e4e7c10
amir <ah.azari.wr@gmail.com>
2024-12-04 21:10:22 +0330

    refactor: Change properties in RequestSession to mutable variables

--- app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestLogin.kt
+++ app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestLogin.kt
@@ -5,9 +5,9 @@ import com.google.gson.annotations.SerializedName
 
 data class RequestLogin(
     @SerializedName("password")
-    val password: String?= null, //
+    var password: String?= null, //
     @SerializedName("request_token")
-    val requestToken: String?= null, // d2ac0a7fb849371d39f3ee84e42786f5db1af8f6
+    var requestToken: String?= null, // d2ac0a7fb849371d39f3ee84e42786f5db1af8f6
     @SerializedName("username")
-    val username: String?= null //
+    var username: String?= null //
 )
--- app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestSession.kt
+++ app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestSession.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 
 data class RequestSession(
     @SerializedName("request_token")
-    val requestToken: String?=null // 1a07dfec25014dc031560f7facc0683fd552e66b
+    var requestToken: String? = null
 )
