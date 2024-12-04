746d4e9f20149ed2b0b015620ebb919d297f9af1
amir <ah.azari.wr@gmail.com>
2024-12-04 09:38:26 +0330

    feat: Add BodyModule for providing RequestLogin and RequestSession dependencies

--- app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestLogin.kt
+++ app/src/main/java/azari/amirhossein/filmora/models/authentication/RequestLogin.kt
@@ -5,9 +5,9 @@ import com.google.gson.annotations.SerializedName
 
 data class RequestLogin(
     @SerializedName("password")
-    val password: String?, //
+    val password: String?= null, //
     @SerializedName("request_token")
-    val requestToken: String?, // d2ac0a7fb849371d39f3ee84e42786f5db1af8f6
+    val requestToken: String?= null, // d2ac0a7fb849371d39f3ee84e42786f5db1af8f6
     @SerializedName("username")
-    val username: String? //
+    val username: String?= null //
 )
