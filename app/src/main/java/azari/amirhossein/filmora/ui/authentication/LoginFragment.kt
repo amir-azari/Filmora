dda9c65c43f624e4fbab0e648fa55083cbe49031
amir <ah.azari.wr@gmail.com>
2024-12-06 18:30:03 +0330

    feat: Add validation and motion layout for login UI
    Implemented input field validation for username and password (checks for empty fields, space in username, and minimum password length).
    Integrated MotionLayout for button animations during login process (transitioning between states based on authentication result).
    Applied Snackbar customization for error messages .

--- app/src/main/java/azari/amirhossein/filmora/ui/authentication/LoginFragment.kt
+++ app/src/main/java/azari/amirhossein/filmora/ui/authentication/LoginFragment.kt
@@ -1,15 +1,22 @@
 package azari.amirhossein.filmora.ui.authentication
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import azari.amirhossein.filmora.R
 import azari.amirhossein.filmora.databinding.FragmentLoginBinding
 import azari.amirhossein.filmora.utils.NetworkRequest
+import azari.amirhossein.filmora.utils.customize
 import azari.amirhossein.filmora.viewmodel.LoginViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,48 +42,118 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            // Set up text watchers
+            etUsername.doAfterTextChanged { validateUsername(it.toString()) }
+            etPassword.doAfterTextChanged { validatePassword(it.toString()) }
+
+            etUsername.setOnEditorActionListener { _, actionId, _ ->
+                handleEditorAction(actionId, etPassword)
+            }
+
+            etPassword.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    btnLogin.performClick()
+                    true
+                } else {
+                    false
+                }
+            }
+
             btnLogin.setOnClickListener {
                 val username = etUsername.text.toString()
                 val password = etPassword.text.toString()
 
-                viewModel.authenticateUser(username, password)
-
-                viewModel.authResult.observe(viewLifecycleOwner) { result ->
-                    when (result) {
-                        is NetworkRequest.Loading -> {
-                            progressbar.visibility = View.VISIBLE
-                            btnLogin.visibility = View.INVISIBLE
-                        }
-
-                        is NetworkRequest.Success -> {
-
-                            progressbar.visibility = View.GONE
-
-                            val sessionId = result.data
-                            if (!sessionId.isNullOrEmpty()) {
-                                //TODO Navigate to Home Fragment
-                            } else {
-                                Toast.makeText(
-                                    requireContext(),
-                                    "Session ID is empty",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-
-                        is NetworkRequest.Error -> {
-                            progressbar.visibility = View.GONE
-                            btnLogin.visibility = View.VISIBLE
-                            Toast.makeText(requireContext(), result.message, Toast.LENGTH_LONG)
-                                .show()
-                        }
+                if (username.isNotEmpty() && password.isNotEmpty()) {
+                    if (validateUsername(username) && validatePassword(password)) {
+                        viewModel.authenticateUser(username, password)
                     }
+                }else {
+                    // Show an error if any field is empty
+                    Snackbar.make(root,R.string.fillRequiredFields , Snackbar.LENGTH_SHORT).customize(
+                        R.color.error,
+                        R.color.white,
+                        Gravity.TOP,
+                    ).show()
                 }
 
+
+
+            }
+
+            viewModel.authResult.observe(viewLifecycleOwner) { result ->
+                when (result) {
+                    is NetworkRequest.Loading -> {
+                        root.transitionToEnd()
+                    }
+
+                    is NetworkRequest.Success -> {
+                        root.transitionToStart()
+
+                        val sessionId = result.data
+                        if (!sessionId.isNullOrEmpty()) {
+                            //TODO Navigate to Home Fragment
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                "Session ID is empty",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    is NetworkRequest.Error -> {
+                        root.transitionToStart()
+                        Snackbar.make(root,result.message.toString() , Snackbar.LENGTH_SHORT).customize(
+                            R.color.error,
+                            R.color.white,
+                            Gravity.TOP,
+                        ).show()
+
+                    }
+                }
             }
 
         }
 
+
+    }
+    private fun validateUsername(username: String): Boolean {
+        return if (!username.contains(" ")) {
+            binding.tilUsername.isErrorEnabled = false
+            true
+        } else {
+            binding.tilUsername.error = if (username.isEmpty()) {
+                getString(R.string.usernameEmpty)
+            } else {
+                getString(R.string.usernameContainsSpace)
+            }
+            false
+        }
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return if (password.isNotEmpty()) {
+            if (password.length >= 4) {
+                binding.tilPassword.isErrorEnabled = false
+                true
+            } else {
+                binding.tilPassword.error = getString(R.string.passwordTooShort)
+                false
+            }
+        } else {
+            binding.tilPassword.isErrorEnabled = false
+            false
+        }
+    }
+    private fun handleEditorAction(actionId: Int, nextField: View): Boolean {
+        return when (actionId) {
+            EditorInfo.IME_ACTION_NEXT -> {
+                nextField.requestFocus()
+                true
+            }
+
+            else -> false
+        }
     }
 
     override fun onDestroyView() {
