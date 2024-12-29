15a6481e3d7bc5c90e2c60526604f37ceb1878e5
amir <ah.azari.wr@gmail.com>
2024-12-29 20:16:14 +0330

    feat: Add genre selection and fetch functionality in TvPreferencesViewModel
    Added _genres LiveData and genres to store and observe TV genres.
    Implemented updateFavoriteGenre and updateDislikedGenre to manage favorite and disliked genres.
    Modified init block to handle fetching TV genres when necessary.
    Introduced LiveData for selected genres (selectedFavoriteGenres, selectedDislikedGenres).

--- app/src/main/java/azari/amirhossein/filmora/viewmodel/TvPreferencesViewModel.kt
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/TvPreferencesViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import azari.amirhossein.filmora.data.SessionManager
 import azari.amirhossein.filmora.data.repository.TvPreferencesRepository
+import azari.amirhossein.filmora.models.prefences.ResponseGenresList
 import azari.amirhossein.filmora.models.prefences.movie.ResponseMoviesList
 import azari.amirhossein.filmora.models.prefences.tv.ResponseTvsList
 import azari.amirhossein.filmora.utils.Event
@@ -35,9 +36,18 @@ class TvPreferencesViewModel @Inject constructor(
     private val _errorMessage = MutableLiveData<Event<String>>()
     val errorMessage: LiveData<Event<String>> = _errorMessage
 
+    private val _genres = MutableLiveData<NetworkRequest<ResponseGenresList>>()
+    val genres: LiveData<NetworkRequest<ResponseGenresList>> = _genres
 
+    private val _selectedFavoriteGenres = MutableLiveData<Set<Int>>(setOf())
+    val selectedFavoriteGenres: LiveData<Set<Int>> = _selectedFavoriteGenres
+
+    private val _selectedDislikedGenres = MutableLiveData<Set<Int>>(setOf())
+    val selectedDislikedGenres: LiveData<Set<Int>> = _selectedDislikedGenres
     init {
         setupSearchQueryFlow()
+        fetchMovieGenres()
+
 
     }
     private fun setupSearchQueryFlow() {
@@ -72,5 +82,43 @@ class TvPreferencesViewModel @Inject constructor(
             currentList.removeAt(position)
             _selectedSeries.value = currentList
         }
+    }
+    private fun fetchMovieGenres() {
+        viewModelScope.launch {
+            repository.getTvGenres()
+                .collect { result ->
+                    _genres.postValue(result)
+                }
+        }
+    }
+
+    fun updateFavoriteGenre(genreId: Int, isSelected: Boolean) {
+        val currentFavorites = _selectedFavoriteGenres.value?.toMutableSet() ?: mutableSetOf()
+        val currentDislikes = _selectedDislikedGenres.value?.toMutableSet() ?: mutableSetOf()
+
+        if (isSelected) {
+            currentFavorites.add(genreId)
+            currentDislikes.remove(genreId)
+        } else {
+            currentFavorites.remove(genreId)
+        }
+
+        _selectedFavoriteGenres.value = currentFavorites
+        _selectedDislikedGenres.value = currentDislikes
+    }
+
+    fun updateDislikedGenre(genreId: Int, isSelected: Boolean) {
+        val currentFavorites = _selectedFavoriteGenres.value?.toMutableSet() ?: mutableSetOf()
+        val currentDislikes = _selectedDislikedGenres.value?.toMutableSet() ?: mutableSetOf()
+
+        if (isSelected) {
+            currentDislikes.add(genreId)
+            currentFavorites.remove(genreId)
+        } else {
+            currentDislikes.remove(genreId)
+        }
+
+        _selectedFavoriteGenres.value = currentFavorites
+        _selectedDislikedGenres.value = currentDislikes
     }
 }
