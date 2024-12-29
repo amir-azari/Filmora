e16186514e90763f0ee4632c4c92e20ef88813c2
amir <ah.azari.wr@gmail.com>
2024-12-29 19:30:54 +0330

    feat: Implement SearchTvPreferencesAdapter for displaying TV search results
    Created SearchTvPreferencesAdapter class for RecyclerView to handle TV series search results.
    Integrated AsyncListDiffer with DiffUtil for efficient list updates.
    Added onItemClick listener to handle item click events.

---
+++ app/src/main/java/azari/amirhossein/filmora/adapter/SearchTvPreferencesAdapter.kt
@@ -0,0 +1,72 @@
+package azari.amirhossein.filmora.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import azari.amirhossein.filmora.databinding.ItemSearchPreferencesBinding
+import azari.amirhossein.filmora.models.prefences.tv.ResponseTvsList
+import coil3.load
+import coil3.request.crossfade
+import javax.inject.Inject
+
+class SearchTvPreferencesAdapter @Inject constructor() :
+    RecyclerView.Adapter<SearchTvPreferencesAdapter.ViewHolder>() {
+
+    var onItemClick: ((ResponseTvsList.Result) -> Unit)? = null
+
+    inner class ViewHolder(private val binding: ItemSearchPreferencesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ResponseTvsList.Result) {
+            binding.apply {
+                val baseUrl = "https://image.tmdb.org/t/p/w500"
+                val fullPosterPath = baseUrl + item.posterPath
+                imgPoster.load(fullPosterPath) {
+                    crossfade(true)
+                    crossfade(400)
+                }
+                txtTitle.text = item.name
+                txtYear.text = item.firstAirDate?.split("-")?.get(0) ?: "N/A"
+                txtRating.text = String.format("%.1f", item.voteAverage)
+
+                root.setOnClickListener {
+                    onItemClick?.invoke(item)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemSearchPreferencesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+    private val diffCallback = object : DiffUtil.ItemCallback<ResponseTvsList.Result>() {
+        override fun areItemsTheSame(
+            oldItem: ResponseTvsList.Result,
+            newItem: ResponseTvsList.Result,
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ResponseTvsList.Result,
+            newItem: ResponseTvsList.Result,
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+}
