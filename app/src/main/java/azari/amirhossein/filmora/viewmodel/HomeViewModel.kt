fc77a56b78f78f1b86be81b673d6f9211716bf5d
amir <ah.azari.wr@gmail.com>
2025-01-07 14:57:44 +0330

    feat: Implement HomeViewModel for managing combined data and random posters
    Created HomeViewModel to manage fetching of combined data using HomeRepository.
    Added live data for holding combined data, random movie poster, and random TV poster.
    Implemented network checking using NetworkChecker to monitor network availability.
    Implemented filtering of trending movies to ensure only valid movie and TV types are included.
    Exposed live data for UI to observe changes in combined data and random posters.

---
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/HomeViewModel.kt
@@ -0,0 +1,68 @@
+package azari.amirhossein.filmora.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import azari.amirhossein.filmora.data.repository.HomeRepository
+import azari.amirhossein.filmora.models.CombinedData
+import azari.amirhossein.filmora.utils.NetworkChecker
+import azari.amirhossein.filmora.utils.NetworkRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: HomeRepository,
+    networkChecker: NetworkChecker
+) : ViewModel() {
+
+    private val _combineData =MutableLiveData<NetworkRequest<CombinedData>>()
+    val combineData: LiveData<NetworkRequest<CombinedData>> = _combineData
+
+    private val _randomMoviePoster = MutableLiveData<String?>()
+    val randomMoviePoster: LiveData<String?> = _randomMoviePoster
+
+    private val _randomTvPoster = MutableLiveData<String?>()
+    val randomTvPoster: LiveData<String?> = _randomTvPoster
+
+    val isNetworkAvailable = networkChecker.startMonitoring()
+
+
+    fun combinedData() {
+        viewModelScope.launch {
+            isNetworkAvailable.collect { isAvailable ->
+                if (isAvailable) {
+                    repository.getCombinedData().collect { result ->
+                        _combineData.postValue(result)
+                        result.data?.let { selectRandomPosters(it) }
+                        filterTrendingMovies(result.data)
+                    }
+                } else {
+                    _combineData.postValue(NetworkRequest.Error("No internet connection"))
+                }
+            }
+        }
+    }
+    private fun selectRandomPosters(data: CombinedData?) {
+        val baseUrl = "https://image.tmdb.org/t/p/original"
+        val movieResults = data?.recommendedMovies?.data?.results.orEmpty()
+        val tvResults = data?.recommendedTvs?.data?.results.orEmpty()
+
+        val randomMoviePoster = movieResults.randomOrNull()?.backdropPath
+        val randomTvPoster = tvResults.randomOrNull()?.backdropPath
+
+        _randomMoviePoster.postValue(randomMoviePoster?.let { baseUrl + it })
+        _randomTvPoster.postValue(randomTvPoster?.let { baseUrl + it })
+    }
+    private fun filterTrendingMovies(data: CombinedData?) {
+        val trendingMovies = data?.trendingMovies?.data?.results.orEmpty()
+        val filteredTrendingMovies = trendingMovies
+            .filter { it.mediaType == "movie" || it.mediaType == "tv" }
+
+        data?.trendingMovies?.data?.results = filteredTrendingMovies
+    }
+}
+
+
