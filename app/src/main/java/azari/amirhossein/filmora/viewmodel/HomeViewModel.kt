dd47810552c9a13066b91bae06c97581608a8fa8
amir <ah.azari.wr@gmail.com>
2025-01-10 20:20:24 +0330

    feat: Implement offline caching and prevent data reload . Added caching mechanism to serve data when the network is unavailable. Prevented unnecessary reloading of data by introducing isDataLoaded flag. Renamed _combineData to _homePageData for clarity.

--- app/src/main/java/azari/amirhossein/filmora/viewmodel/HomeViewModel.kt
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/HomeViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import azari.amirhossein.filmora.data.repository.HomeRepository
-import azari.amirhossein.filmora.models.CombinedData
+import azari.amirhossein.filmora.models.HomePageData
 import azari.amirhossein.filmora.utils.Constants
 import azari.amirhossein.filmora.utils.NetworkChecker
 import azari.amirhossein.filmora.utils.NetworkRequest
@@ -19,8 +19,8 @@ class HomeViewModel @Inject constructor(
     networkChecker: NetworkChecker
 ) : ViewModel() {
 
-    private val _combineData =MutableLiveData<NetworkRequest<CombinedData>>()
-    val combineData: LiveData<NetworkRequest<CombinedData>> = _combineData
+    private val _homePageData =MutableLiveData<NetworkRequest<HomePageData>>()
+    val homePageData: LiveData<NetworkRequest<HomePageData>> = _homePageData
 
     private val _randomMoviePoster = MutableLiveData<String?>()
     val randomMoviePoster: LiveData<String?> = _randomMoviePoster
@@ -29,24 +29,28 @@ class HomeViewModel @Inject constructor(
     val randomTvPoster: LiveData<String?> = _randomTvPoster
 
     val isNetworkAvailable = networkChecker.startMonitoring()
+    private var isDataLoaded = false
 
 
     fun combinedData() {
+        if (isDataLoaded) return
+
         viewModelScope.launch {
             isNetworkAvailable.collect { isAvailable ->
                 if (isAvailable) {
-                    repository.getCombinedData().collect { result ->
-                        _combineData.postValue(result)
+                    repository.getRemoteData().collect { result ->
+                        _homePageData.postValue(result)
                         result.data?.let { selectRandomPosters(it) }
                         filterTrendingMovies(result.data)
+                        isDataLoaded = true
                     }
                 } else {
-                    _combineData.postValue(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+                    _homePageData.postValue(repository.getCachedData())
                 }
             }
         }
     }
-    private fun selectRandomPosters(data: CombinedData?) {
+    private fun selectRandomPosters(data: HomePageData?) {
         val baseUrl = Constants.Network.IMAGE_BASE_URL
         val movieResults = data?.recommendedMovies?.data?.results.orEmpty()
         val tvResults = data?.recommendedTvs?.data?.results.orEmpty()
@@ -57,12 +61,12 @@ class HomeViewModel @Inject constructor(
         _randomMoviePoster.postValue(randomMoviePoster?.let { baseUrl + Constants.ImageSize.ORIGINAL+ it })
         _randomTvPoster.postValue(randomTvPoster?.let { baseUrl + Constants.ImageSize.ORIGINAL+ it })
     }
-    private fun filterTrendingMovies(data: CombinedData?) {
-        val trendingMovies = data?.trendingMovies?.data?.results.orEmpty()
+    private fun filterTrendingMovies(data: HomePageData?) {
+        val trendingMovies = data?.trending?.data?.results.orEmpty()
         val filteredTrendingMovies = trendingMovies
             .filter { it.mediaType == "movie" || it.mediaType == "tv" }
 
-        data?.trendingMovies?.data?.results = filteredTrendingMovies
+        data?.trending?.data?.results = filteredTrendingMovies
     }
 }
 
