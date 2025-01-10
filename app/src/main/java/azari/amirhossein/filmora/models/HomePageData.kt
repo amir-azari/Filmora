d854e10b7d7066a758ee7a4999993a934f6a487a
amir <ah.azari.wr@gmail.com>
2025-01-10 10:48:43 +0330

    refactor: Rename CombinedData to HomePageData for clarity

--- app/src/main/java/azari/amirhossein/filmora/models/HomePageData.kt
+++ app/src/main/java/azari/amirhossein/filmora/models/HomePageData.kt
@@ -6,8 +6,8 @@ import azari.amirhossein.filmora.models.prefences.movie.ResponseMoviesList
 import azari.amirhossein.filmora.models.prefences.tv.ResponseTvsList
 import azari.amirhossein.filmora.utils.NetworkRequest
 
-data class CombinedData(
-    val trendingMovies: NetworkRequest<ResponseTrendingList>,
+data class HomePageData(
+    val trending: NetworkRequest<ResponseTrendingList>,
     val movieGenres: NetworkRequest<ResponseGenresList>,
     val tvGenres: NetworkRequest<ResponseGenresList>,
     val recommendedMovies: NetworkRequest<ResponseMoviesList>,
