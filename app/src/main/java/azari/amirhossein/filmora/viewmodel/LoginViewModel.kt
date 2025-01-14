3caa75f8569acd74cfb8407859b66c32abfe783d
amir <ah.azari.wr@gmail.com>
2025-01-14 11:59:09 +0330

    refactor: Migrate LoginViewModel to StateFlow and improve network handling
    Replaced MutableLiveData with MutableStateFlow for better state management.
    Added Event wrapper to handle single-use events in flows.
    Introduced observeNetworkStatus to monitor network connectivity and emit errors when offline.
    Validated network availability before executing authenticateUser and authenticateGuest.
    Added _isSessionActive StateFlow to track session status with checkSessionStatus.
    Improved lifecycle management by stopping network monitoring in onCleared.
    Enhanced readability and maintainability with onEach and launchIn for flow operations.

--- app/src/main/java/azari/amirhossein/filmora/viewmodel/LoginViewModel.kt
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/LoginViewModel.kt
@@ -1,14 +1,18 @@
 package azari.amirhossein.filmora.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import azari.amirhossein.filmora.data.SessionManager
 import azari.amirhossein.filmora.data.repository.LoginRepository
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.Event
 import azari.amirhossein.filmora.utils.NetworkChecker
 import azari.amirhossein.filmora.utils.NetworkRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,40 +20,80 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val repository: LoginRepository,
     private val networkChecker: NetworkChecker,
-    private val sessionManager: SessionManager
 
-) : ViewModel() {
+    ) : ViewModel() {
 
-    private val _authResult = MutableLiveData<NetworkRequest<String>?>()
-    val authResult: MutableLiveData<NetworkRequest<String>?> get() = _authResult
+    private val _authResult = MutableStateFlow<Event<NetworkRequest<String>>?>(null)
+    val authResult: StateFlow<Event<NetworkRequest<String>>?> get() = _authResult
 
-    val isNetworkAvailable = networkChecker.startMonitoring()
+    private val _isSessionActive = MutableStateFlow(false)
+    val isSessionActive: StateFlow<Boolean> get() = _isSessionActive
 
+    init {
+        networkChecker.startMonitoring()
+        observeNetworkStatus()
+    }
+
+    private fun observeNetworkStatus() {
+        viewModelScope.launch {
+            networkChecker.isNetworkAvailable.collect { isAvailable ->
+                if (!isAvailable) {
+                    _authResult.value = Event(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+                }
+            }
+        }
+    }
     // Authenticate user
     fun authenticateUser(username: String, password: String) {
         viewModelScope.launch {
-            repository.authenticateUser(username, password)
-                .collect { result ->
-                    _authResult.postValue(result)
-                }
+            if (networkChecker.isNetworkAvailable.value) {
+                repository.authenticateUser(username, password)
+                    .onEach { result ->
+                        _authResult.value = Event(result)
+                    }
+                    .launchIn(viewModelScope)
+            } else {
+                _authResult.value =
+                    Event(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+            }
+
+
         }
     }
 
     // Authenticate guest
     fun authenticateGuest() {
         viewModelScope.launch {
-            repository.authenticateGuest()
-                .collect { result ->
-                    _authResult.postValue(result)
-                }
+            if (networkChecker.isNetworkAvailable.value) {
+                repository.authenticateGuest()
+                    .onEach { result ->
+                        _authResult.value = Event(result)
+                    }
+                    .launchIn(viewModelScope)
+            } else {
+                _authResult.value =
+                    Event(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+            }
+
         }
     }
 
-    fun clearAuthResult() {
-        _authResult.value = null
+    fun checkSessionStatus() {
+        viewModelScope.launch {
+            repository.checkSessionStatus()
+                .onEach { status ->
+                    _isSessionActive.value = status
+                }
+                .launchIn(viewModelScope)
+        }
     }
 
     suspend fun getSessionId(): String? {
         return repository.getSessionId().firstOrNull()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        networkChecker.stopMonitoring()
     }
 }
