aecbf2dbfc932a2601442542a365200a6cfb0c86
amir <ah.azari.wr@gmail.com>
2025-01-16 11:07:13 +0330

    feat: Implement DetailsMovieViewModel
    - Fetches media details based on ID and type.
    - Handles network availability and displays appropriate messages.
    - Uses `DetailsRepository` to retrieve data.
    - Emits results as `NetworkRequest` wrapped in an `Event`.

---
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/DetailsMovieViewModel.kt
@@ -0,0 +1,72 @@
+package azari.amirhossein.filmora.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import azari.amirhossein.filmora.data.repository.DetailsRepository
+import azari.amirhossein.filmora.models.home.HomeMediaItem
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.Event
+import azari.amirhossein.filmora.utils.NetworkChecker
+import azari.amirhossein.filmora.utils.NetworkRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsMovieViewModel @Inject constructor(
+    private val repository: DetailsRepository,
+    private val networkChecker: NetworkChecker,
+) : ViewModel() {
+
+    private val _mediaDetails  = MutableStateFlow<Event<NetworkRequest<HomeMediaItem>>?>(null)
+    val mediaDetails: StateFlow<Event<NetworkRequest<HomeMediaItem>>?> = _mediaDetails
+
+    private var lastRequestedMediaId: Int? = null
+    private var lastRequestedMediaType: String? = null
+    init {
+        networkChecker.startMonitoring()
+        monitorNetworkChanges()
+    }
+
+    private fun monitorNetworkChanges() {
+        viewModelScope.launch {
+            networkChecker.isNetworkAvailable.collect { isAvailable ->
+                if (isAvailable) {
+                    lastRequestedMediaId?.let { id ->
+                        lastRequestedMediaType?.let { type ->
+                            getMediaDetails(id, type)
+                        }
+                    }
+                } else {
+                    _mediaDetails.value =
+                        Event(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+                }
+            }
+        }
+    }
+
+    fun getMediaDetails(id: Int, type: String) {
+        viewModelScope.launch {
+            lastRequestedMediaId = id
+            lastRequestedMediaType = type
+
+            if (networkChecker.isNetworkAvailable.value) {
+                repository.getMediaDetails(id, type)
+                    .collect { result ->
+                        _mediaDetails.value = Event(result)
+                    }
+            } else {
+                _mediaDetails.value =
+                    Event(NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION))
+            }
+        }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        networkChecker.stopMonitoring()
+    }
+}
