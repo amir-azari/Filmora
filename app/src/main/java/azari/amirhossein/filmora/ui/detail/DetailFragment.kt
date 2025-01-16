341a077af653c38a112786264042a6c6dd72f4ae
amir <ah.azari.wr@gmail.com>
2025-01-16 12:08:58 +0330

    feat: Design and implement UI for DetailFragment
    - Created the layout for the detail screen using ConstraintLayout and NestedScrollView.
    - Added views for displaying backdrop image, poster image, title, rating, vote count, genres, and overview.
    - Implemented expand/collapse functionality for the overview section.
    - Added loading and error states for handling data loading and network errors.
    - Defined color resources for light and dark themes.
    - Added drawable resources for icons and placeholders.

---
+++ app/src/main/java/azari/amirhossein/filmora/ui/detail/DetailFragment.kt
@@ -0,0 +1,66 @@
+package azari.amirhossein.filmora.ui.detail
+
+import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.res.colorResource
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import azari.amirhossein.filmora.R
+import azari.amirhossein.filmora.adapter.GenresAdapter
+import azari.amirhossein.filmora.databinding.FragmentDetailBinding
+import azari.amirhossein.filmora.models.detail.ResponseMovieDetails
+import azari.amirhossein.filmora.models.detail.ResponseTvDetails
+import azari.amirhossein.filmora.models.home.HomeMediaItem
+import azari.amirhossein.filmora.models.prefences.ResponseGenresList
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkRequest
+import azari.amirhossein.filmora.utils.customize
+import azari.amirhossein.filmora.viewmodel.DetailsMovieViewModel
+import coil3.load
+import coil3.request.crossfade
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+    //Binding
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
