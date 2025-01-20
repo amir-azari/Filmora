a95f2215d050057574313f590c007fb83102c6aa
amir <ah.azari.wr@gmail.com>
2025-01-20 14:42:49 +0330

    feat: Enhance Navigation and UI in MainActivity
    - Added `handleDestinationChanges` to manage UI state based on navigation destination.
    - Implemented fade animations for showing and hiding the bottom navigation bar.
    - Improved visibility of toolbar menu items based on navigation destination.
    - Added tvDetailsFragment to hiddenFragments list.
    - Modified the creation of the `NavHostFragment` to handle nullability.

--- app/src/main/java/azari/amirhossein/filmora/ui/MainActivity.kt
+++ app/src/main/java/azari/amirhossein/filmora/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package azari.amirhossein.filmora.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
@@ -7,13 +10,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import azari.amirhossein.filmora.R
 import azari.amirhossein.filmora.databinding.ActivityMainBinding
+import azari.amirhossein.filmora.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,8 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
-    private lateinit var navHost : NavHostFragment
-
+    private lateinit var navHost: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -43,7 +48,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+                ?: NavHostFragment.create(R.navigation.nav_main).also {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, it)
+                        .setReorderingAllowed(true)
+                        .commitNow()
+                }
         navController = navHost.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -61,61 +73,69 @@ class MainActivity : AppCompatActivity() {
 
         // Handle destination changes
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.homeFragment -> {
-                    showBottomNav()
-                    showToolbar()
-                    showProfileSection()
-                    hideToolbarTitle()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-                }
-                R.id.movieFragment,
-                R.id.tvFragment,
-                R.id.peopleFragment -> {
-                    showBottomNav()
-                    showToolbar()
-                    hideProfileSection()
-                    showToolbarTitle()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-                }
-                R.id.detailFragment -> {
-                    hideBottomNav()
-                    showToolbar()
-                    hideProfileSection()
-                    showToolbarTitle()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-                    invalidateOptionsMenu()
-                }
-                R.id.splashFragment,
-                R.id.loginFragment,
-                R.id.moviePreferencesFragment,
-                R.id.tvPreferencesFragment,
-                R.id.webViewFragment -> {
-                    hideBottomNav()
-                    hideToolbar()
-                }
-            }
+            handleDestinationChanges(destination.id)
         }
-
     }
 
+    private fun handleDestinationChanges(destinationId: Int) {
+        when (destinationId) {
+            R.id.homeFragment -> {
+                showBottomNavWithFade()
+                showToolbar()
+                showProfileSection()
+                hideToolbarTitle()
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            }
+
+            R.id.movieFragment, R.id.tvFragment, R.id.peopleFragment -> {
+                showBottomNavWithFade()
+                showToolbar()
+                hideProfileSection()
+                showToolbarTitle()
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            }
+
+            R.id.movieDetailFragment, R.id.tvDetailsFragment -> {
+                hideBottomNavWithFade()
+                showToolbar()
+                hideProfileSection()
+                showToolbarTitle()
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            }
+
+            R.id.splashFragment, R.id.loginFragment, R.id.moviePreferencesFragment,
+            R.id.tvPreferencesFragment, R.id.webViewFragment -> {
+                hideBottomNav()
+                hideToolbar()
+            }
+        }
+        invalidateOptionsMenu()
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
-
-        if (navController.currentDestination?.id == R.id.detailFragment) {
-            for (i in 0 until menu.size()) {
-                menu.getItem(i).isVisible = false
-            }
-        }
         return true
     }
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        val hiddenFragments = listOf(R.id.movieDetailFragment, R.id.tvDetailsFragment)
+        val currentDestinationId = navController.currentDestination?.id
+
+        for (i in 0 until menu.size()) {
+            menu.getItem(i).isVisible = currentDestinationId !in hiddenFragments
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+
+
     private fun showProfileSection() {
-        binding.profileSection.visibility = View.VISIBLE
+            binding.profileSection.visibility = View.VISIBLE
+
     }
 
     private fun hideProfileSection() {
-        binding.profileSection.visibility = View.GONE
+            binding.profileSection.visibility = View.GONE
+
     }
 
     private fun hideToolbarTitle() {
@@ -125,22 +145,56 @@ class MainActivity : AppCompatActivity() {
     private fun showToolbarTitle() {
         supportActionBar?.setDisplayShowTitleEnabled(true)
     }
+
     private fun hideToolbar() {
-        binding.toolbar.visibility = View.GONE
+            binding.toolbar.visibility = View.GONE
+
     }
 
     private fun showToolbar() {
-        binding.toolbar.visibility = View.VISIBLE
+            binding.toolbar.visibility = View.VISIBLE
+
     }
 
     private fun hideBottomNav() {
-        binding.bottomNav.visibility = View.GONE
+            binding.bottomNav.visibility = View.GONE
+
     }
+
     private fun showBottomNav() {
-        binding.bottomNav.visibility = View.VISIBLE
+            binding.bottomNav.visibility = View.VISIBLE
+
     }
+
+    private fun hideBottomNavWithFade() {
+        if (binding.bottomNav.visibility != View.GONE && binding.bottomNav.alpha == 1f) {
+            ObjectAnimator.ofFloat(binding.bottomNav, "alpha", 1f, 0f).apply {
+                duration = 150
+                addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        binding.bottomNav.visibility = View.GONE
+                        binding.bottomNav.alpha = 1f
+                    }
+                })
+                start()
+            }
+        }
+    }
+
+
+    private fun showBottomNavWithFade() {
+        if (binding.bottomNav.visibility != View.VISIBLE) {
+
+            binding.bottomNav.visibility = View.VISIBLE
+            ObjectAnimator.ofFloat(binding.bottomNav, "alpha", 0f, 1f).apply {
+                duration = 150
+                start()
+            }
+        }
+    }
+
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
 }
