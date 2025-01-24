ceda1495a5a28ce7b8e34575d088b202f7876995
amir <ah.azari.wr@gmail.com>
2025-01-24 20:49:14 +0330

    feat: Implement Similar and Recommendation in DetailsViewModel
    - Added StateFlows for Similar and Recommendation for movie and tv show in `DetailsViewModel`.
    - Added `updateMediaDetails` method to update StateFlows with data.

--- app/src/main/java/azari/amirhossein/filmora/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/DetailsViewModel.kt
@@ -3,8 +3,11 @@ package azari.amirhossein.filmora.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import azari.amirhossein.filmora.data.repository.DetailsRepository
-import azari.amirhossein.filmora.models.ResponseLanguage
 import azari.amirhossein.filmora.models.detail.DetailMediaItem
+import azari.amirhossein.filmora.models.detail.ResponseMovieRecommendations
+import azari.amirhossein.filmora.models.detail.ResponseMovieSimilar
+import azari.amirhossein.filmora.models.detail.ResponseTvRecommendations
+import azari.amirhossein.filmora.models.detail.ResponseTvSimilar
 import azari.amirhossein.filmora.utils.Constants
 import azari.amirhossein.filmora.utils.Event
 import azari.amirhossein.filmora.utils.NetworkChecker
@@ -24,6 +27,19 @@ class DetailsViewModel @Inject constructor(
     private val _mediaDetails = MutableStateFlow<Event<NetworkRequest<DetailMediaItem>>>(Event(NetworkRequest.Loading()))
     val mediaDetails: StateFlow<Event<NetworkRequest<DetailMediaItem>>?> = _mediaDetails
 
+    private val _movieSimilar = MutableStateFlow<NetworkRequest<ResponseMovieSimilar>>(NetworkRequest.Loading())
+    val movieSimilar: StateFlow<NetworkRequest<ResponseMovieSimilar>> = _movieSimilar
+
+    private val _movieRecommendations = MutableStateFlow<NetworkRequest<ResponseMovieRecommendations>>(NetworkRequest.Loading())
+    val movieRecommendations: StateFlow<NetworkRequest<ResponseMovieRecommendations>> = _movieRecommendations
+
+    private val _tvSimilar = MutableStateFlow<NetworkRequest<ResponseTvSimilar>>(NetworkRequest.Loading())
+    val tvSimilar: StateFlow<NetworkRequest<ResponseTvSimilar>> = _tvSimilar
+
+    private val _tvRecommendations = MutableStateFlow<NetworkRequest<ResponseTvRecommendations>>(NetworkRequest.Loading())
+    val tvRecommendations: StateFlow<NetworkRequest<ResponseTvRecommendations>> = _tvRecommendations
+
+
     private var lastRequestedMediaId: Int? = null
     private var lastRequestedMediaType: String? = null
 
@@ -31,6 +47,15 @@ class DetailsViewModel @Inject constructor(
         networkChecker.startMonitoring()
         monitorNetworkChanges()
     }
+
+
+    fun updateMediaDetails(mediaItem: DetailMediaItem) {
+        mediaItem.similar?.let { _movieSimilar.value = NetworkRequest.Success(it) }
+        mediaItem.recommendations?.let { _movieRecommendations.value = NetworkRequest.Success(it) }
+        mediaItem.tvSimilar?.let { _tvSimilar.value = NetworkRequest.Success(it) }
+        mediaItem.tvRecommendations?.let { _tvRecommendations.value = NetworkRequest.Success(it) }
+    }
+
 
     private fun monitorNetworkChanges() {
         viewModelScope.launch {
