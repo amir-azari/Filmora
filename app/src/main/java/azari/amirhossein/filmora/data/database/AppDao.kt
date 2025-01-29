b403de17874a80ee7093346362a2dcb95002beb8
amir <ah.azari.wr@gmail.com>
2025-01-29 18:18:06 +0330

    feat: Implement Detail Data in Room Database
    - Added methods to `AppDao` for handling detail data:
      - `getDetailById`: Retrieve detail data by ID.
      - `saveDetail`: Save detail data.
      - `deleteExpiredDetailData`: Delete expired detail data based on timestamp.
    - Added `DetailEntity` for storing detail data.

--- app/src/main/java/azari/amirhossein/filmora/data/database/AppDao.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/database/AppDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import azari.amirhossein.filmora.data.database.entity.DetailEntity
 import azari.amirhossein.filmora.data.database.entity.HomeEntity
 import azari.amirhossein.filmora.utils.Constants
 import kotlinx.coroutines.flow.Flow
@@ -16,4 +17,14 @@ interface AppDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveHomeData(data: HomeEntity)
+
+    // Detail
+    @Query("SELECT * FROM ${Constants.Database.DETAIL_TABLE} WHERE id = :id")
+    fun getDetailById(id: Int): Flow<DetailEntity?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveDetail(detail: DetailEntity)
+
+    @Query("DELETE FROM ${Constants.Database.DETAIL_TABLE} WHERE timestamp < :expirationTime")
+    suspend fun deleteExpiredDetailData(expirationTime: Long)
 }
--- app/src/main/java/azari/amirhossein/filmora/data/database/AppDatabase.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/database/AppDatabase.kt
@@ -3,10 +3,13 @@ package azari.amirhossein.filmora.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import azari.amirhossein.filmora.data.database.entity.DetailEntity
 import azari.amirhossein.filmora.data.database.entity.HomeEntity
 
-@Database(entities = [HomeEntity::class], version = 3, exportSchema = false)
+
+@Database(entities = [HomeEntity::class , DetailEntity::class], version = 4, exportSchema = false)
 @TypeConverters(ResponseTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun combinedDataDao(): AppDao
+    abstract fun ado(): AppDao
 }
+
