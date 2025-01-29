43237edca06aba8fca1d439f83b9109f4fc0acc5
amir <ah.azari.wr@gmail.com>
2025-01-29 18:17:07 +0330

    feat: Add DetailEntity for caching movie and TV show details
    Created `DetailEntity` to store details like credits, language, similar, recommendations, videos, images, and reviews for movies and TV shows in the local database.

---
+++ app/src/main/java/azari/amirhossein/filmora/data/database/entity/DetailEntity.kt
@@ -0,0 +1,34 @@
+package azari.amirhossein.filmora.data.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import azari.amirhossein.filmora.models.ResponseLanguage
+import azari.amirhossein.filmora.models.detail.ResponseCredit
+import azari.amirhossein.filmora.models.detail.ResponseImage
+import azari.amirhossein.filmora.models.detail.ResponseMovieDetails
+import azari.amirhossein.filmora.models.detail.ResponseMovieRecommendations
+import azari.amirhossein.filmora.models.detail.ResponseMovieSimilar
+import azari.amirhossein.filmora.models.detail.ResponseReviews
+import azari.amirhossein.filmora.models.detail.ResponseTvDetails
+import azari.amirhossein.filmora.models.detail.ResponseTvRecommendations
+import azari.amirhossein.filmora.models.detail.ResponseTvSimilar
+import azari.amirhossein.filmora.models.detail.ResponseVideo
+import azari.amirhossein.filmora.utils.Constants
+
+@Entity(tableName = Constants.Database.DETAIL_TABLE)
+data class DetailEntity(
+    @PrimaryKey val id: Int = 0,
+    val movie: ResponseMovieDetails? = null,
+    val tv: ResponseTvDetails? = null,
+    val credits: ResponseCredit? = null,
+    val language: ResponseLanguage? = null,
+    val similar: ResponseMovieSimilar? = null,
+    val recommendations: ResponseMovieRecommendations? = null,
+    val tvSimilar: ResponseTvSimilar? = null,
+    val tvRecommendations: ResponseTvRecommendations? = null,
+    val videos: ResponseVideo? = null,
+    val images: ResponseImage? = null,
+    val reviews: ResponseReviews? =null,
+    val timestamp: Long = System.currentTimeMillis()
+
+)
