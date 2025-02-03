c36e8a356fa81868ab1c52f4ca7230431502f254
amir <ah.azari.wr@gmail.com>
2025-02-03 08:35:37 +0330

    feat: Add MayLikeTvsViewModel for managing TV shows data
    - Created `MayLikeTvsViewModel` to handle fetching and managing TV show data.
    - Implemented network monitoring to fetch data when online and handle offline state using cached data.
    - Used `PagingData` to display data efficiently.
    - Implemented logic for handling network availability.
    - Added `NetworkChecker` dependency for checking network connection.
    - Add `cachedIn` function to save the data in memory.
    - Clear cached data and network monitoring in `onCleared`.

---
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/MayLikeTvsViewModel.kt
@@ -0,0 +1,72 @@
+package azari.amirhossein.filmora.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import azari.amirhossein.filmora.data.repository.MayLikeTvsRepository
+import azari.amirhossein.filmora.models.prefences.movie.ResponseMoviesList
+import azari.amirhossein.filmora.models.prefences.tv.ResponseTvsList
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkChecker
+import azari.amirhossein.filmora.utils.NetworkRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MayLikeTvsViewModel @Inject constructor(
+    private val repository: MayLikeTvsRepository,
+    private val networkChecker: NetworkChecker
+) : ViewModel() {
+
+    private val _tvs = MutableStateFlow<NetworkRequest<PagingData<ResponseTvsList.Result>>>(NetworkRequest.Loading())
+    val tvs: StateFlow<NetworkRequest<PagingData<ResponseTvsList.Result>>> = _tvs
+
+    private var cachedTvs: PagingData<ResponseTvsList.Result>? = null
+
+    init {
+        observeNetworkAndFetchMovies()
+    }
+
+    private fun observeNetworkAndFetchMovies() {
+        viewModelScope.launch {
+            networkChecker.startMonitoring().collect { isAvailable ->
+                if (isAvailable) {
+                    fetchMovies()
+                } else {
+                    handleOfflineState()
+                }
+            }
+        }
+    }
+
+
+    private fun fetchMovies() {
+        viewModelScope.launch {
+            repository.getTvs()
+                .cachedIn(viewModelScope) // Cache the flow
+                .collectLatest { result ->
+                    cachedTvs = result
+                    _tvs.value = NetworkRequest.Success(result)
+                }
+        }
+    }
+
+    private fun handleOfflineState() {
+        if (cachedTvs != null) {
+            _tvs.value = NetworkRequest.Success(cachedTvs!!)
+        } else {
+            _tvs.value = NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION)
+        }
+    }
+    override fun onCleared() {
+        super.onCleared()
+        cachedTvs = null
+        networkChecker.stopMonitoring()
+    }
+}
+
