82c47359f3c396c486ff4bde7e9db17133be6a7b
amir <ah.azari.wr@gmail.com>
2025-02-03 08:35:04 +0330

    feat: Implement MayLikeTvsRepository and MayLikeTvsPagingSource
    - Created `MayLikeTvsRepository` to handle fetching "may like" TV shows.
    - Implemented `MayLikeTvsPagingSource` for paginated loading of TV shows.
    - Used `Pager` to configure and provide the paginated TV show data as a `Flow`.
    - Utilized `SessionManager` and `RemoteDataSource` to manage session and retrieve data from remote source.

---
+++ app/src/main/java/azari/amirhossein/filmora/data/repository/MayLikeTvsRepository.kt
@@ -0,0 +1,37 @@
+package azari.amirhossein.filmora.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import azari.amirhossein.filmora.data.SessionManager
+import azari.amirhossein.filmora.data.source.RemoteDataSource
+import azari.amirhossein.filmora.models.prefences.movie.ResponseMoviesList
+import azari.amirhossein.filmora.models.prefences.tv.ResponseTvsList
+import azari.amirhossein.filmora.paging.MayLikeMoviesPagingSource
+import azari.amirhossein.filmora.paging.MayLikeTvsPagingSource
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class MayLikeTvsRepository @Inject constructor(
+    private val sessionManager: SessionManager,
+    private val remote: RemoteDataSource
+) {
+    fun getTvs(): Flow<PagingData<ResponseTvsList.Result>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false,
+                initialLoadSize = 20
+            ),
+            pagingSourceFactory = { MayLikeTvsPagingSource(remote, sessionManager) }
+        ).flow
+            .flowOn(Dispatchers.IO)
+    }
+}
+
