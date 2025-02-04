eeaf7e310fa04728a98b92af2ae9b50918ff5cb5
amir <ah.azari.wr@gmail.com>
2025-02-04 17:01:22 +0330

    feat: Implement MovieViewModel for managing movie data
    - Created `MovieViewModel` to handle fetching and managing movie data from `MovieRepository`.
    - Integrated `NetworkChecker` to monitor network availability.
    - Implemented logic to retrieve and update `MoviePageData` using `StateFlow`.
    - Added functionality to select and expose a random movie poster URL.
    - Handled network availability changes and updated the data accordingly.
    - Stopped network monitoring on view model clear.

---
+++ app/src/main/java/azari/amirhossein/filmora/viewmodel/MovieViewModel.kt
@@ -0,0 +1,78 @@
+package azari.amirhossein.filmora.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import azari.amirhossein.filmora.data.repository.MovieRepository
+import azari.amirhossein.filmora.models.HomePageData
+import azari.amirhossein.filmora.models.movie.MoviePageData
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkChecker
+import azari.amirhossein.filmora.utils.NetworkRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieViewModel @Inject constructor(
+    private val repository: MovieRepository,
+    private val networkChecker: NetworkChecker,
+) : ViewModel() {
+    private val _moviePageData by lazy {
+        MutableStateFlow<NetworkRequest<MoviePageData>?>(null)
+    }
+    val moviePageData: StateFlow<NetworkRequest<MoviePageData>?> = _moviePageData
+
+    private val _randomMoviePoster = MutableStateFlow<String?>(null)
+    val randomMoviePoster: StateFlow<String?> = _randomMoviePoster
+    init {
+        networkChecker.startMonitoring()
+        monitorNetworkChanges()
+
+    }
+    private fun monitorNetworkChanges() {
+        viewModelScope.launch {
+            networkChecker.isNetworkAvailable.collect { isAvailable ->
+                if (isAvailable) handleOnlineState()
+
+            }
+        }
+    }
+
+    //-----Api-----
+    private fun handleOnlineState() {
+        repository.getRemoteData()
+            .catch { error ->
+                _moviePageData.value = NetworkRequest.Error(error.message ?: Constants.Message.NO_INTERNET_CONNECTION)
+            }
+            .onEach { result ->
+                updateHomePageData(result)
+                result.data?.let {
+                    selectRandomPosters(it)
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun updateHomePageData(newData: NetworkRequest<MoviePageData>) {
+        if (_moviePageData.value != newData) {
+            _moviePageData.value = newData
+        }
+    }
+
+    private fun selectRandomPosters(data: MoviePageData?) {
+        val baseUrl = Constants.Network.IMAGE_BASE_URL
+        _randomMoviePoster.value = data?.trending?.data?.results
+            ?.randomOrNull()?.backdropPath
+            ?.let { "$baseUrl${Constants.ImageSize.ORIGINAL}$it" }
+
+    }
+    override fun onCleared() {
+        super.onCleared()
+        networkChecker.stopMonitoring()
+    }
+}
