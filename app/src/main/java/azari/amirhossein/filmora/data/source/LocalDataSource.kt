ca079d20b186f3f99f5279aff1e82e9e1f768cb6
amir <ah.azari.wr@gmail.com>
2025-02-06 14:15:20 +0330

    feat: Add local data source for movie data
    - Implemented `saveMovieData` and `getMovieData` in `LocalDataSource` for saving and retrieving `MovieEntity`.
    - Modified `LocalDataSource` to include new functions for movie-related operations.

--- app/src/main/java/azari/amirhossein/filmora/data/source/LocalDataSource.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/source/LocalDataSource.kt
@@ -3,11 +3,16 @@ package azari.amirhossein.filmora.data.source
 import azari.amirhossein.filmora.data.database.AppDao
 import azari.amirhossein.filmora.data.database.entity.DetailEntity
 import azari.amirhossein.filmora.data.database.entity.HomeEntity
+import azari.amirhossein.filmora.data.database.entity.MovieEntity
 import javax.inject.Inject
 class LocalDataSource @Inject constructor(private val dao: AppDao) {
     // Home
     suspend fun saveHomeData(entity: HomeEntity) = dao.saveHomeData(entity)
     fun getHomeData() = dao.getHomeData()
+
+    // Movie
+    suspend fun saveMovieData(entity: MovieEntity) = dao.saveMovieData(entity)
+    fun getMovieData() = dao.getMovieData()
 
     // Detail
     suspend fun saveDetail(entity: DetailEntity) = dao.saveDetail(entity)
