b8faf9202d525fbe6fab1155d45f4c9f8c46892f
amir <ah.azari.wr@gmail.com>
2025-02-06 14:15:36 +0330

    feat: Implement local caching for movie data in MovieRepository
    - Added methods to convert `MoviePageData` to `MovieEntity` and vice-versa for database operations.
    - Implemented `saveMovieData` in local data source to cache remote movie data.
    - Added `getCachedData` to retrieve cached movie data from local storage, returning an error if no data is present.
    - Updated `fetchData` to save fetched remote movie data to the local database.

--- app/src/main/java/azari/amirhossein/filmora/data/repository/MovieRepository.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/repository/MovieRepository.kt
@@ -1,13 +1,17 @@
 package azari.amirhossein.filmora.data.repository
 
+import azari.amirhossein.filmora.data.database.entity.HomeEntity
+import azari.amirhossein.filmora.data.database.entity.MovieEntity
 import azari.amirhossein.filmora.data.source.LocalDataSource
 import azari.amirhossein.filmora.data.source.RemoteDataSource
+import azari.amirhossein.filmora.models.HomePageData
 import azari.amirhossein.filmora.models.movie.MoviePageData
 import azari.amirhossein.filmora.utils.Constants
 import azari.amirhossein.filmora.utils.NetworkRequest
 import azari.amirhossein.filmora.utils.NetworkResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
@@ -20,6 +24,7 @@ class MovieRepository @Inject constructor(
         emit(NetworkRequest.Loading())
         try {
             val remoteData = fetchRemoteData()
+            local.saveMovieData(remoteData.toEntity())
             emit(NetworkRequest.Success(remoteData))
         } catch (e: Exception) {
             emit(NetworkRequest.Error(e.localizedMessage ?: Constants.Message.UNKNOWN_ERROR))
@@ -27,8 +32,7 @@ class MovieRepository @Inject constructor(
         }
     }.flowOn(Dispatchers.IO)
 
-    private suspend fun fetchRemoteData(
-    ): MoviePageData {
+    private suspend fun fetchRemoteData(): MoviePageData {
         // Call APIs
         val trending = remote.getTrendingMovie("day")
         val movieGenres = remote.getMovieGenres()
@@ -46,5 +50,40 @@ class MovieRepository @Inject constructor(
             topRated = NetworkResponse(topRated).handleNetworkResponse(),
             upcoming = NetworkResponse(upcoming).handleNetworkResponse()
             )
+    }
+
+    private fun MoviePageData.toEntity() = MovieEntity(
+        trending = trending.data!!,
+        movieGenres = movieGenres.data!!,
+        popular = popular.data!!,
+        nowPlaying = nowPlaying.data!!,
+        topRated = topRated.data!!,
+        upcoming = upcoming.data!!
+
+    )
+
+
+    private fun MovieEntity.toCombinedData() = MoviePageData(
+        trending = NetworkRequest.Success(this.trending),
+        movieGenres = NetworkRequest.Success(this.movieGenres),
+        popular = NetworkRequest.Success(this.popular),
+        nowPlaying = NetworkRequest.Success(this.nowPlaying),
+        topRated = NetworkRequest.Success(this.topRated),
+        upcoming = NetworkRequest.Success(this.upcoming)
+    )
+
+    // Get cached data
+    suspend fun getCachedData(): NetworkRequest<MoviePageData> {
+        return try {
+            // Try to get cached data
+            val cachedData = local.getMovieData().firstOrNull()
+            if (cachedData != null) {
+                NetworkRequest.Success(cachedData.toCombinedData())
+            } else {
+                NetworkRequest.Error(Constants.Message.NO_INTERNET_CONNECTION)
+            }
+        } catch (e: Exception) {
+            NetworkRequest.Error("Failed to load cached data: ${e.localizedMessage}")
+        }
     }
 }
