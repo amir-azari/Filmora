6c7ab33a13c694e7f44ab093e866ae86f71a61cf
amir <ah.azari.wr@gmail.com>
2025-02-07 18:25:41 +0330

    feat: Implement People data persistence in LocalDataSource
    - Added `savePeopleData` and `getPeopleData` to `LocalDataSource` for persisting and retrieving `PeopleEntity`.
    - Added PeopleEntity to the project.

--- app/src/main/java/azari/amirhossein/filmora/data/source/LocalDataSource.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/source/LocalDataSource.kt
@@ -4,6 +4,7 @@ import azari.amirhossein.filmora.data.database.AppDao
 import azari.amirhossein.filmora.data.database.entity.DetailEntity
 import azari.amirhossein.filmora.data.database.entity.HomeEntity
 import azari.amirhossein.filmora.data.database.entity.MovieEntity
+import azari.amirhossein.filmora.data.database.entity.PeopleEntity
 import javax.inject.Inject
 class LocalDataSource @Inject constructor(private val dao: AppDao) {
     // Home
@@ -14,6 +15,9 @@ class LocalDataSource @Inject constructor(private val dao: AppDao) {
     suspend fun saveMovieData(entity: MovieEntity) = dao.saveMovieData(entity)
     fun getMovieData() = dao.getMovieData()
 
+    // People
+    suspend fun savePeopleData(entity: PeopleEntity) = dao.savePeopleData(entity)
+    fun getPeopleData() = dao.getPeopleData()
     // Detail
     suspend fun saveDetail(entity: DetailEntity) = dao.saveDetail(entity)
     fun getDetailById(id: Int) = dao.getDetailById(id)
