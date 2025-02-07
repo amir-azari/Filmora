ae6b40448ed71b6e6f2ccd9025d08c26e32842b4
amir <ah.azari.wr@gmail.com>
2025-02-07 17:43:31 +0330

    feat: Implement PeopleRepository and PeoplePageData
    - Created `PeoplePageData` to encapsulate popular celebrity data.
    - Implemented `PeopleRepository` to fetch popular celebrity data from the remote data source.
    - Added `getRemoteData` method to `PeopleRepository` for handling network requests and responses.
    - Implemented `fetchRemoteData` to make API calls for popular people.

---
+++ app/src/main/java/azari/amirhossein/filmora/data/repository/PeopleRepository.kt
@@ -0,0 +1,41 @@
+package azari.amirhossein.filmora.data.repository
+
+import azari.amirhossein.filmora.data.source.LocalDataSource
+import azari.amirhossein.filmora.data.source.RemoteDataSource
+import azari.amirhossein.filmora.models.celebtiry.PeoplePageData
+import azari.amirhossein.filmora.models.movie.MoviePageData
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkRequest
+import azari.amirhossein.filmora.utils.NetworkResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class PeopleRepository @Inject constructor(
+    private val remote: RemoteDataSource,
+    private val local: LocalDataSource
+) {
+    fun getRemoteData(): Flow<NetworkRequest<PeoplePageData>> = flow {
+        emit(NetworkRequest.Loading())
+        try {
+            val remoteData = fetchRemoteData()
+            emit(NetworkRequest.Success(remoteData))
+        } catch (e: Exception) {
+            emit(NetworkRequest.Error(e.localizedMessage ?: Constants.Message.UNKNOWN_ERROR))
+
+        }
+    }.flowOn(Dispatchers.IO)
+
+    private suspend fun fetchRemoteData(): PeoplePageData {
+        // Call APIs
+        val popular1 = remote.getPopularPeoples(1)
+
+
+
+        return PeoplePageData(
+            popular1 = NetworkResponse(popular1).handleNetworkResponse(),
+        )
+    }
+}
---
+++ app/src/main/java/azari/amirhossein/filmora/models/celebtiry/PeoplePageData.kt
@@ -0,0 +1,7 @@
+package azari.amirhossein.filmora.models.celebtiry
+
+import azari.amirhossein.filmora.utils.NetworkRequest
+
+class PeoplePageData (
+    val popular1 : NetworkRequest<ResponsePopularCelebrity>,
+)
