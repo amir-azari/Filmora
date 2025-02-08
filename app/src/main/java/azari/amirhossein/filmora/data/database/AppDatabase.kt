dc0935774095dad629f9b225cfd5626eebcbeea0
amir <ah.azari.wr@gmail.com>
2025-02-08 14:34:39 +0330

    feat: Add TvEntity and update database version
    - Added `TvEntity` to the database.
    - Updated database version to 7.

--- app/src/main/java/azari/amirhossein/filmora/data/database/AppDatabase.kt
+++ app/src/main/java/azari/amirhossein/filmora/data/database/AppDatabase.kt
@@ -7,11 +7,12 @@ import azari.amirhossein.filmora.data.database.entity.DetailEntity
 import azari.amirhossein.filmora.data.database.entity.HomeEntity
 import azari.amirhossein.filmora.data.database.entity.MovieEntity
 import azari.amirhossein.filmora.data.database.entity.PeopleEntity
+import azari.amirhossein.filmora.data.database.entity.TvEntity
 
 
 @Database(
-    entities = [HomeEntity::class, MovieEntity::class, DetailEntity::class , PeopleEntity::class],
-    version = 6,
+    entities = [HomeEntity::class, MovieEntity::class, DetailEntity::class , PeopleEntity::class , TvEntity::class],
+    version = 7,
     exportSchema = false
 )
 @TypeConverters(ResponseTypeConverter::class)
