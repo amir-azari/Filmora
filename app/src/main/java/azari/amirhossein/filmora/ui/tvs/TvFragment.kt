a0bafb8841cf62996027c882a0004450226eda09
amir <ah.azari.wr@gmail.com>
2025-02-08 13:45:59 +0330

    feat: Implement TvFragment UI and data handling
    - Implemented `TvFragment` to display TV show data.
    - Added `TrendingTvAdapter` for displaying trending TV shows.
    - Implemented RecyclerView setup and data observation for the trending TV list.
    - Integrated `TvViewModel` to fetch and manage TV show data.
    - Added error handling and loading states.
    - Displayed random movie posters using `coil3`.
    - Added `showErrorSnackbar` method for showing error messages.
    - Added `showLoading`, `showSuccess` and `showError` methods for showing views with different states.

--- app/src/main/java/azari/amirhossein/filmora/ui/tvs/TvFragment.kt
+++ app/src/main/java/azari/amirhossein/filmora/ui/tvs/TvFragment.kt
@@ -1,14 +1,30 @@
 package azari.amirhossein.filmora.ui.tvs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import azari.amirhossein.filmora.R
-import azari.amirhossein.filmora.databinding.FragmentPeopleBinding
+import azari.amirhossein.filmora.adapter.TrendingTvAdapter
 import azari.amirhossein.filmora.databinding.FragmentTvBinding
+import azari.amirhossein.filmora.utils.Constants
+import azari.amirhossein.filmora.utils.NetworkRequest
+import azari.amirhossein.filmora.utils.customize
+import azari.amirhossein.filmora.viewmodel.TvViewModel
+import coil3.load
+import coil3.request.crossfade
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class TvFragment : Fragment() {
@@ -16,6 +32,11 @@ class TvFragment : Fragment() {
     private var _binding: FragmentTvBinding? = null
     private val binding get() = _binding!!
 
+    // ViewModel
+    private val viewModel: TvViewModel by viewModels()
+
+    @Inject
+    lateinit var trendingAdapter : TrendingTvAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
         // Inflate the layout for this fragment
@@ -24,6 +45,108 @@ class TvFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerViews()
+        observeViewModel()
+    }
+    // Setup recyclerView
+    private fun setupRecyclerViews() {
+        binding.apply {
+            rvTrending.apply {
+                layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                adapter = trendingAdapter
+                setHasFixedSize(true)
+            }
+        }
+
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tvPageData.collect { state ->
+                    binding.mainContentContainer.visibility = View.GONE
+                    if (state != null) {
+                        when (state) {
+                            is NetworkRequest.Loading -> {
+                                showLoading()
+                            }
+
+                            is NetworkRequest.Success -> {
+                                showSuccess()
+                                state.data?.let { data ->
+                                    // Update adapters with the new data
+                                    trendingAdapter.differ.submitList(data.trending.data?.results)
+
+
+                                    data.tvGenres.data?.genres?.let { genres ->
+                                        trendingAdapter.submitGenres(genres)
+
+                                    }
+                                }
+                            }
+
+                            is NetworkRequest.Error -> {
+                                showError()
+                                if (state.message == Constants.Message.NO_INTERNET_CONNECTION) {
+                                    binding.internetLay.visibility = View.VISIBLE
+                                }
+                                showErrorSnackbar(binding.root, state.message.toString())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Observing random movie
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.randomMoviePoster.collect { url ->
+                    loadImage(url, binding.imgMoviePoster)
+                }
+
+            }
+        }
+
+    }
+
+    private fun loadImage(url: String?, imageView: ImageView) {
+        imageView.load(url) {
+            crossfade(true)
+            crossfade(400)
+        }
+    }
+
+    private fun showErrorSnackbar(root: View, message: String) {
+        Snackbar.make(root, message, Snackbar.LENGTH_SHORT).apply {
+            customize(R.color.error, R.color.white, Gravity.TOP)
+            show()
+        }
+    }
+
+    private fun showLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.mainContentContainer.visibility = View.GONE
+        binding.internetLay.visibility  =View.GONE
+
+
+    }
+
+    private fun showSuccess() {
+        binding.progressBar.visibility = View.GONE
+        binding.mainContentContainer.visibility = View.VISIBLE
+
+
+    }
+
+    private fun showError() {
+        binding.progressBar.visibility = View.GONE
+        binding.mainContentContainer.visibility = View.GONE
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
