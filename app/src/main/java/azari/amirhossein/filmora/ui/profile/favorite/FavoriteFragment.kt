ddeb0d8d654752ded70e0da7e52d0736f192053c
amir <ah.azari.wr@gmail.com>
2025-02-23 18:09:17 +0330

    feat: Implement Favorite Fragment with Movie and TV Show Tabs
    - Created `FavoriteFragment` with a `TabLayout` and `ViewPager2` to switch between favorite movies and TV shows.
    - Added `FavoriteMovieFragment` to display the list of favorite movies.
    - Added `FavoriteTvsFragment` to display the list of favorite TV shows.
    - Implemented `FavoriteViewPagerAdapter` for handling fragments within the `ViewPager2`.
    - Added logic to switch between Movie and Tv tabs.
    - Implemented RecyclerView setup and data observation for movies and tvs fragments.
    - Implemented error handling and no data messages.
    - Implemented click listeners for navigate to detail page.

---
+++ app/src/main/java/azari/amirhossein/filmora/ui/profile/favorite/FavoriteFragment.kt
@@ -0,0 +1,83 @@
+package azari.amirhossein.filmora.ui.profile.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import azari.amirhossein.filmora.R
+import azari.amirhossein.filmora.adapter.FavoriteViewPagerAdapter
+import azari.amirhossein.filmora.databinding.FragmentFavoriteBinding
+import azari.amirhossein.filmora.viewmodel.FavoriteViewModel
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: FavoriteViewModel by viewModels()
+    private lateinit var viewPagerAdapter: FavoriteViewPagerAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPager()
+        observeCurrentTab()
+    }
+
+    private fun setupViewPager() {
+        viewPagerAdapter = FavoriteViewPagerAdapter(this)
+        binding.viewPager.apply {
+            adapter = viewPagerAdapter
+            offscreenPageLimit = 1
+            setPageTransformer { page, position ->
+                page.alpha = 1 - kotlin.math.abs(position)
+            }
+            setCurrentItem(viewModel.currentTabPosition.value, false)
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.movies)
+                1 -> getString(R.string.tv_shows)
+                else -> ""
+            }
+        }.attach()
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                viewModel.updateTabPosition(position)
+            }
+        })
+    }
+
+
+    private fun observeCurrentTab() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.currentTabPosition.collectLatest { position ->
+                binding.viewPager.currentItem = position
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+
