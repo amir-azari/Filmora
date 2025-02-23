5b3b2315f735d7e3ff626f646c51f0fabb1c75a0
amir <ah.azari.wr@gmail.com>
2025-02-23 19:37:54 +0330

    feat: Add RatedViewPagerAdapter for managing RatedMovie and RatedTvShow fragments
    - Created `RatedViewPagerAdapter` to handle the display of `RatedMovieFragment` and `RatedTvShowFragment`.
    - Implemented `getItemCount`, `createFragment`, `getItemId`, and `containsItem` to manage the fragments within a ViewPager2.

---
+++ app/src/main/java/azari/amirhossein/filmora/adapter/RatedViewPagerAdapter.kt
@@ -0,0 +1,32 @@
+package azari.amirhossein.filmora.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import azari.amirhossein.filmora.ui.profile.rated.RatedMovieFragment
+import azari.amirhossein.filmora.ui.profile.rated.RatedTvShowFragment
+
+class RatedViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> RatedMovieFragment()
+            1 -> RatedTvShowFragment()
+            else -> throw IllegalArgumentException("Invalid position $position")
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return when (position) {
+            0 -> 500L
+            1 -> 501L
+            else -> position.toLong()
+        }
+    }
+
+    override fun containsItem(itemId: Long): Boolean {
+        return itemId == 500L || itemId == 501L
+    }
+}
+
