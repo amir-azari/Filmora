7da1b2c5bd06a18b04f5eb45da540999121aad4f
amir <ah.azari.wr@gmail.com>
2025-02-25 21:48:23 +0330

    feat: Add login required dialog and handle guest user navigation in ProfileFragment
    - Created `dialog_message.xml` for displaying dialog messages.
    - Implemented `showLoginDialog` to prompt guest users to log in.
    - Added `handleNavigationOrShowDialog` to check if the user is a guest before navigating.
    - Modified `ProfileFragment` to handle guest user states (visibility of UI elements).
    - Changed the onClickListeners for Favorite, Watchlist and Rating items to show dialog when user is guest.

--- app/src/main/java/azari/amirhossein/filmora/ui/profile/ProfileFragment.kt
+++ app/src/main/java/azari/amirhossein/filmora/ui/profile/ProfileFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -11,22 +14,26 @@ import androidx.navigation.fragment.findNavController
 import azari.amirhossein.filmora.R
 import azari.amirhossein.filmora.data.SessionManager
 import azari.amirhossein.filmora.databinding.FragmentProfileBinding
+import azari.amirhossein.filmora.models.detail.RateRequest
 import azari.amirhossein.filmora.utils.Constants
 import azari.amirhossein.filmora.utils.NetworkRequest
 import azari.amirhossein.filmora.viewmodel.SharedAccountViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
-    //Binding
-    private var _binding : FragmentProfileBinding? = null
+    // Binding
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     @Inject
     lateinit var sessionManager: SessionManager
 
     private val accountViewModel: SharedAccountViewModel by viewModels()
+    private var isGuestUser: Boolean = true
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -40,6 +47,7 @@ class ProfileFragment : Fragment() {
 
         viewLifecycleOwner.lifecycleScope.launch {
             sessionManager.isGuest().collect { isGuest ->
+                isGuestUser = isGuest
                 if (isGuest) {
                     binding.tvUsername.visibility = View.GONE
                     binding.btnLogin.visibility = View.VISIBLE
@@ -61,29 +69,52 @@ class ProfileFragment : Fragment() {
         }
 
         binding.btnLogin.setOnClickListener {
-            val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment(loginType =  Constants.LoginType.PROFILE)
+            val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment(
+                loginType = Constants.LoginType.PROFILE
+            )
             findNavController().navigate(action)
         }
 
-        binding.cvFavorite.setOnClickListener {
+        binding.cvFavorite.setOnClickListener { handleNavigationOrShowDialog {
             val action = ProfileFragmentDirections.actionProfileFragmentToFavoriteFragment()
             findNavController().navigate(action)
-        }
+        }}
 
-        binding.cvWatchlist.setOnClickListener {
+        binding.cvWatchlist.setOnClickListener { handleNavigationOrShowDialog {
             val action = ProfileFragmentDirections.actionProfileFragmentToWatchlistFragment()
             findNavController().navigate(action)
-        }
+        }}
 
-        binding.cvRatings.setOnClickListener {
+        binding.cvRatings.setOnClickListener { handleNavigationOrShowDialog {
             val action = ProfileFragmentDirections.actionProfileFragmentToRatedFragment()
             findNavController().navigate(action)
+        }}
+    }
+
+    private fun handleNavigationOrShowDialog(navigateAction: () -> Unit) {
+        if (isGuestUser) {
+            showLoginDialog()
+        } else {
+            navigateAction()
         }
+    }
+
+    private fun showLoginDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_message, null)
+        val tvMessage = dialogView.findViewById<TextView>(R.id.tvDialogMessage)
+
+        tvMessage.text = getString(R.string.dialog_login_required_message)
+
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        dialog.show()
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
