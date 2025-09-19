d1af13eb48fe170c99ea81815ecd0fc800d7b71e
amir <ah.azari.wr@gmail.com>
2025-09-19 18:15:30 +0330

    feat(main): Setup MainActivity and main app screens

--- app/src/main/java/azari/amirhossein/filmora/ui/celebreties/CelebritiesFragment.kt
+++ app/src/main/java/azari/amirhossein/filmora/ui/celebreties/CelebritiesFragment.kt
@@ -1,4 +1,4 @@
-package azari.amirhossein.filmora.ui.profile
+package azari.amirhossein.filmora.ui.celebreties
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,14 +9,13 @@ import azari.amirhossein.filmora.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileFragment : Fragment() {
+class CelebritiesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_celebrities, container, false)
     }
-
 }
